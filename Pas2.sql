b7a34b85e2bb6badce072f72a8f9c9bed44dae48
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-04-22 13:56:19 +0200

    Añadidos comentarios

--- Pas2.sql
+++ Pas2.sql
@@ -1,4 +1,4 @@
-/*Pas 2*/
+/* CREACION DE TABLA activitats_net */
 CREATE TABLE IF NOT EXISTS activitats_net (
     id_activitat INT NOT NULL,
     id_usuari INT NOT NULL,
@@ -8,10 +8,11 @@ CREATE TABLE IF NOT EXISTS activitats_net (
     tipus_activitat VARCHAR(50) NOT NULL,
     calories INT NOT NULL,
     dispositiu VARCHAR(20) NOT NULL,
-    fin_de_semana BOOLEAN NOT NULL,
+    fin_de_semana BOOLEAN NOT NULL, /* CELDA BOOLEANA FIN DE SEMANA */
     PRIMARY KEY (id_activitat)
 );
 
+/* PROCEDEMIENTO PARA COPIAR LOS VALORES DE LA TABLA activitats_raw Y PEGARLO EN activitats_net ADEMAS DE LLENAR LA COLUMNA FIN DE SEMANA */
 DELIMITER $$
 DROP PROCEDURE IF EXISTS mover$$
 CREATE PROCEDURE mover()
@@ -29,7 +30,7 @@ BEGIN
 END $$
 DELIMITER ;
 
-
+/* EVENTO QUE SE EJECUTA TODOS LOS DIAS A LAS 00:00 QUE LLAMA AL PROCEDIMIENTO mover */
 DELIMITER $$
 DROP EVENT IF EXISTS activitat_moved$$
 CREATE EVENT activitat_moved
