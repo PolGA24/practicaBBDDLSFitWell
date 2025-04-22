a3ccb7dbd5b2ce6a4a3e73ef36ffd1645a94d732
Gerard Fornés <gerard.fornes@gracia.lasalle.cat>
2025-04-22 12:58:52 +0200

    create table activitats_raw

--- create.sql
+++ create.sql
@@ -1,2 +1,14 @@
 CREATE DATABASE IF NOT EXISTS lsfitwell;
 USE lsfitwell;
+
+CREATE TABLE IF NOT EXISTS activitats_raw (
+    id_activitat INT NOT NULL AUTO_INCREMENT,
+    id_usuari INT NOT NULL,
+    data_activitat DATE NOT NULL,
+    hora_inici TIME NOT NULL,
+    durada_minuts INT NOT NULL,
+    tipus_activitat VARCHAR(50) NOT NULL,
+    calories INT NOT NULL,
+    dispositiu VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id_activitat),
+)
