0b79420ff75170fb97621c62b4982f1a3aa53814
Gerard Fornés <gerard.fornes@gracia.lasalle.cat>
2025-04-24 10:36:35 +0200

    pas 5

--- Archivos sql/Pas5.sql
+++ Archivos sql/Pas5.sql
@@ -1,11 +1,35 @@
 /*Pas 5*/
 /* Procedure que detecti i afegeixi nous registres no existents a MD_activitat*/
 DELIMITER $$
-
 DROP PROCEDURE IF EXISTS afegir_registres$$
-CREATE PROCEDURE afegir_registres()
+CREATE PROCEDURE afegir_registres(
+    IN vid_activitat INT,
+    IN vnom_activitat VARCHAR(50)
+)
+
 BEGIN
+    DECLARE vexisteix INT DEFAULT 0;
+
+    SELECT COUNT(*) INTO vexisteix
+    FROM MD_activitat
+    WHERE id_activitat = vid_activitat;
+
+    IF vexisteix = 0 THEN
+        INSERT INTO MD_activitat (id_activitat, nom, descripcio)
+        VALUES (vid_activitat, LOWER(vnom_activitat), CONCAT('Descripcio de ', LOWER(vnom_activitat)));
+    END IF;
     
 END $$
+DELIMITER ;
 
+
+DELIMITER $$
+DROP TRIGGER IF EXISTS trg_afegir_registres$$
+CREATE TRIGGER trg_afegir_registres
+AFTER INSERT ON activitats_net
+FOR EACH ROW
+
+BEGIN
+    CALL afegir_registres(NEW.id_activitat, NEW.tipus_activitat);
+END $$
 DELIMITER ;
