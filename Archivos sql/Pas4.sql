f6955ea74bc49661807c258fdcbb8e55f47fa642
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-04-24 09:33:47 +0200

    Pas 4 Foreign key

--- Archivos sql/Pas4.sql
+++ Archivos sql/Pas4.sql
@@ -1,8 +1,9 @@
 /*Pas 4*/
 CREATE TABLE IF NOT EXISTS MD_activitat (
     id INT NOT NULL AUTO_INCREMENT,
+    id_activitat INT,
     nom VARCHAR(50),
     descripcio VARCHAR(500),
-    FOREIGN KEY (id) REFERENCES activitats_net(id_activitat)
+    FOREIGN KEY (id_activitat) REFERENCES activitats_net(id_activitat)
 
 );
