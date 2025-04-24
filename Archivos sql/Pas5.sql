ebccd03b5122d8d4179da91d564adfdda95394c9
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-04-24 10:31:06 +0200

    Modifico nombre procedure

--- Archivos sql/Pas5.sql
+++ Archivos sql/Pas5.sql
@@ -2,8 +2,8 @@
 /* Procedure que detecti i afegeixi nous registres no existents a MD_activitat*/
 DELIMITER $$
 
-DROP PROCEDURE IF EXISTS afegir_registres$$
-CREATE PROCEDURE afegir_registres()
+DROP PROCEDURE IF EXISTS manteniment_activitats$$
+CREATE PROCEDURE manteniment_activitats()
 BEGIN
     
 END $$
