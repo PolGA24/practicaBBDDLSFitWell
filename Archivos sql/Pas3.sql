973ed14b35938a05d4a84a1718a6630ebcb73253
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-04-24 09:39:14 +0200

    Ajustes paso 3

--- Archivos sql/Pas3.sql
+++ Archivos sql/Pas3.sql
@@ -1,50 +1,54 @@
 /*Pas 3*/
 
 /* Creació de la taula */
-CREATE TABLE control_carguas (
+DROP TABLE IF EXISTS control_carregues;
+CREATE TABLE control_carregues (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_fichero VARCHAR(255) NOT NULL,
-    archivos_introducidos INT NOT NULL,
-    data_carga DATETIME DEFAULT CURRENT_TIMESTAMP
+    nom_fitxer VARCHAR(255) NOT NULL,
+    files_inserides INT NOT NULL,
+    data_carrega DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 /* Modificar el procedure de càrrega */
 DELIMITER $$
 
-CREATE PROCEDURE cargar_actividades_net(nombre_fichero VARCHAR(255))
+DROP PROCEDURE IF EXISTS cargar_activitats_net$$
+CREATE PROCEDURE cargar_activitats_net(nom_fitxer VARCHAR(255))
 BEGIN
     DECLARE num_files INT;
 
-    -- Inserim les dades a activitats_net
-    INSERT INTO actividades_net (
-        id_usuario, data_actividades, hora_inicio, duracion_minutos,
-        tipos_actividades, calorias, dispositivos, fin_de_semana
+    -- Inserir dades a activitats_net
+    INSERT INTO activitats_net (
+        id_usuari, data_activitat, hora_inici, durada_minuts,
+        tipus_activitat, calories, dispositiu, fin_de_semana
     )
-    SELECT id_usuario, data_actividades, hora_inicio, duracion_minutos,
-            tipos_actividades, calorias, dispositivos,
-            CASE 
-                WHEN DAYOFWEEK(fechaa_actividad) IN (1, 7) THEN TRUE
-                ELSE FALSE
-            END AS fin_de_semana
+    SELECT id_usuari, data_activitat, hora_inici, durada_minuts,
+           tipus_activitat, calories, dispositiu,
+           CASE 
+               WHEN DAYOFWEEK(data_activitat) IN (1, 7) THEN TRUE
+               ELSE FALSE
+           END
     FROM activitats_raw;
 
-    -- Comptem les files afegides
+    -- Comptar files inserides
     SET num_files = ROW_COUNT();
 
-    -- Inserim el registre a la taula de control
-    INSERT INTO control_cargas (nombre_ficher, archivos_introducidos)
-    VALUES (nombre_archivos, num_files);
+    -- Actualitzar taula de control
+    INSERT INTO control_carregues (nom_fitxer, files_inserides)
+    VALUES (nom_fitxer, num_files);
 END $$
 
 DELIMITER ;
 
+
 /* Procedure per exportar la taula control_carregues a CSV */
 DELIMITER $$
 
+DROP PROCEDURE IF EXISTS exportar_control_carregues$$
 CREATE PROCEDURE exportar_control_carregues()
 BEGIN
     SELECT * 
-    INTO OUTFILE 'C:/Users/Xavier Fornes Bort/Documents/GitHub/practicaBBDDLSFitWell/Archivos cvs/control_carregues.csv'
+    INTO OUTFILE '/ruta/on/guardar/control_carregues.csv'
     FIELDS TERMINATED BY ';'
     ENCLOSED BY '"'
     LINES TERMINATED BY '\n'
