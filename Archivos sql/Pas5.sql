b0e5dbea170b8ed987f4461846ca9923d4fe0471
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-04-28 09:20:48 +0200

    Paso 5

--- Archivos sql/Pas5.sql
+++ Archivos sql/Pas5.sql
@@ -1,33 +1,40 @@
 /*Pas 5*/
 /* Procedure que detecti i afegeixi nous registres no existents a MD_activitat*/
-DELIMITER $$
-DROP PROCEDURE IF EXISTS afegir_registres$$
-CREATE PROCEDURE afegir_registres(
-    IN vid_activitat INT,
-    IN vnom_activitat VARCHAR(50)
-)
-
-BEGIN
-    DECLARE vexisteix INT DEFAULT 0;
-
-    SELECT COUNT(*) INTO vexisteix FROM MD_activitat
-        WHERE id_activitat = vid_activitat;
-
-    IF vexisteix = 0 THEN
-        INSERT INTO MD_activitat (id_activitat, nom, descripcio)
-        VALUES (vid_activitat, LOWER(vnom_activitat), CONCAT('Descripcio de ', LOWER(vnom_activitat)));
-    END IF;
-    
-END $$
-DELIMITER ;
-
 
 DELIMITER $$
-DROP TRIGGER IF EXISTS trg_afegir_registres$$
-CREATE TRIGGER trg_afegir_registres
-AFTER INSERT ON activitats_net
-FOR EACH ROW
+DROP PROCEDURE IF EXISTS insertar_noves_activitats$$
+CREATE PROCEDURE insertar_noves_activitats()
 BEGIN
-    CALL afegir_registres(NEW.id_activitat, NEW.tipus_activitat);
-END $$
+    DECLARE done BOOLEAN DEFAULT FALSE;
+    DECLARE v_id_activitat INT(11);
+    DECLARE v_nom VARCHAR(50);
+    DECLARE v_descripcio VARCHAR(500);
+    DECLARE c_activitats CURSOR FOR SELECT id_activitat, nom, descripcio FROM md_activitat;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    OPEN c_activitats;
+    activitats_loop: LOOP
+        FETCH c_activitats INTO v_id_activitat, v_nom, v_descripcio;
+        IF done THEN
+            LEAVE activitats_loop;
+        END IF;
+
+        IF v_nom IS NULL OR v_nom = '' THEN
+            SET v_nom = 'activitat desconeguda';
+        END IF;
+
+        IF v_descripcio IS NULL OR v_descripcio = '' THEN
+            SET v_descripcio = 'sense descripció';
+        END IF;
+
+        SET v_nom = LOWER(v_nom);
+
+        IF NOT EXISTS (SELECT 1 FROM MD_activitat WHERE id_activitat = v_id_activitat) THEN
+            INSERT INTO MD_activitat (id_activitat, nom, descripcio)
+            VALUES (v_id_activitat, v_nom, v_descripcio);
+        END IF;
+
+    END LOOP activitats_loop;
+    CLOSE c_activitats;
+END$$
 DELIMITER ;
