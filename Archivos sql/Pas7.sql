25a179cbff592c48d371c6c79f8c0860edd74e3e
Gerard Fornés <gerard.fornes@gracia.lasalle.cat>
2025-05-01 21:33:34 +0200

    pas 7 finalitzat

--- Archivos sql/Pas7.sql
+++ Archivos sql/Pas7.sql
@@ -1,40 +1,52 @@
-/*Pas 7*/ /*¡¡CAL REVISAR!!*/
-DELIMITER $$
+-- PAS 7
 
-CREATE EVENT DuplicateTablesEvent
-ON SCHEDULE EVERY 1 WEEK
-STARTS '2025-01-05 23:00:00'
-ENDS '2025-12-28 23:00:00'
+CREATE DATABASE IF NOT EXISTS lsfitwell_backup;
+
+SET GLOBAL event_scheduler = ON;
+
+DELIMITER $$
+DROP EVENT IF EXISTS event_backup_setmanal$$
+CREATE EVENT event_backup_setmanal
+ON SCHEDULE
+    EVERY 1 WEEK
+    STARTS '2025-01-05 23:00:00'
+    ENDS '2025-12-28 23:00:00'
 DO
 BEGIN
     DECLARE done INT DEFAULT FALSE;
-    DECLARE tableName VARCHAR(255);
-    DECLARE cur CURSOR FOR 
-        SELECT table_name 
-        FROM information_schema.tables 
-        WHERE table_schema = DATABASE();
+    DECLARE nom_taula VARCHAR(64);
+    DECLARE nom_nou_taula VARCHAR(128);
+    DECLARE sql_copia TEXT;
+    DECLARE data_sufix VARCHAR(8);
+
+    SET data_sufix = DATE_FORMAT(CURDATE(), '%Y%m%d');
+
+    DECLARE cur CURSOR FOR
+        SELECT table_name
+        FROM information_schema.tables
+        WHERE table_schema = 'lsfitwell';
+
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
     OPEN cur;
 
-    read_loop: LOOP
-        FETCH cur INTO tableName;
+    bucle_taules: LOOP
+        FETCH cur INTO nom_taula;
         IF done THEN
-            LEAVE read_loop;
+            LEAVE bucle_taules;
         END IF;
 
-        SET @query = CONCAT('CREATE TABLE ', tableName, '_', DATE_FORMAT(NOW(), '%Y%m%d'), ' LIKE ', tableName);
-        PREPARE stmt FROM @query;
-        EXECUTE stmt;
-        DEALLOCATE PREPARE stmt;
+        SET nom_nou_taula = CONCAT(nom_taula, '_', data_sufix);
 
-        SET @query = CONCAT('INSERT INTO ', tableName, '_', DATE_FORMAT(NOW(), '%Y%m%d'), ' SELECT * FROM ', tableName);
-        PREPARE stmt FROM @query;
+        SET sql_copia = CONCAT(
+            'CREATE TABLE lsfitwell_backup.', nom_nou_taula,
+            ' AS SELECT * FROM lsfitwell.', nom_taula
+        );
+        PREPARE stmt FROM sql_copia;
         EXECUTE stmt;
         DEALLOCATE PREPARE stmt;
     END LOOP;
 
     CLOSE cur;
 END$$
-
 DELIMITER ;
